9d110d073ad23c4173505234c6a0571c3ca47f9d
ikemura-ren <utip.saikai.nozomu@icloud.com>
2022-10-27 23:49:12 +0900

    変更

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 )
+
+type String string
+
+func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, s)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
+func handler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, 世界")
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+  	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+	http.HandleFunc("/test", handler2) 
+	http.Handle("/tes", String("こんにちは"))  
 	http.ListenAndServe(":8080", nil)
 }
