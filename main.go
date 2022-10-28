7128bea1e32fcd063fca217d59cbbf2a2b80ec98
ikemura-ren <utip.saikai.nozomu@icloud.com>
2022-10-28 09:18:32 +0900

    初期に戻す

--- main.go
+++ main.go
@@ -5,23 +5,11 @@ import (
 	"net/http"
 )
 
-type String string
-
-func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, s)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
+func FirstHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
-func handler2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, 世界")
-}
-
 func main() {
-  	http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-	http.HandleFunc("/test", handler2) 
-	http.Handle("/tes", String("こんにちは"))  
+	http.HandleFunc("/", FirstHandler)
 	http.ListenAndServe(":8080", nil)
 }
