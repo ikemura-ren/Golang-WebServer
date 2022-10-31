1ebcf5e27e57c3c7113ba0c878c06b432067c2e9
ikemura-ren <utip.saikai.nozomu@icloud.com>
2022-10-31 18:19:37 +0900

    GETで値を送って返すハンドラの作成

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func FirstHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World")
 }
 
+func SecondHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello" + r.FormValue("hoge"))
+}
+
 func main() {
 	http.HandleFunc("/", FirstHandler) // Hello, Worldを返すハンドラ
+	http.HandleFunc("/second", SecondHandler) // 値を送ってHello + [param]を返すハンドラ（GET）
 	http.ListenAndServe(":8080", nil)
 }
